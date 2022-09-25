90672638209db27f811bf9d175bc0b2a44e46e49
Victor <vleunti@gmail.com>
2022-09-25 19:34:00 +0200

    save sandbox.js

---
+++ chapter1/sandbox.js
@@ -0,0 +1,21 @@
+//alert('hello, world');
+//console.log(1);
+//console.log(2);
+
+let age = 25;
+let year = 2022;
+
+console.log(age,year);
+
+age = 14;
+
+console.log(age);
+
+const points = 100;
+//points = 50; // error cannot be changed
+console.log(points);
+
+var score = 75;
+
+console.log(score);
+
